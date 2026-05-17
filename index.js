e7291c8a90049801ddadc3c267e53b78ca0d3711
salman <salmanibnriyaj@gmail.com>
2026-05-18 03:01:02 +0600

    server connected

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const dotenv = require ('dotenv');
+dotenv.config()
+const cors = require('cors')
+const port = process.env.PORT
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
