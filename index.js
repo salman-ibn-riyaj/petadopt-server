0cfedb99780c4af59257a9a9ad28cbb308b175cb
salman <salmanibnriyaj@gmail.com>
2026-05-18 03:19:11 +0600

    mongo db made connect

--- index.js
+++ index.js
@@ -3,9 +3,47 @@ const app = express()
 const dotenv = require ('dotenv');
 dotenv.config()
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT
 app.use(cors())
 app.use(express.json())
+
+
+
+
+
+const uri = process.env.MONGODB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+  
+    await client.connect();
+
+    const database = client.db("petAdopt");
+    const petsCollection = database.collection("pets");
+
+    
+
+
+   
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
